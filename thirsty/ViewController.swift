15b7a5023fd6625b73c3b0730f11b8ad14b7e461
Gianni Settino <g@miltonandparc.com>
2015-10-19 14:58:39 -0400

    Initial Commit

---
+++ thirsty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  thirsty
+//
+//  Created by Gianni Settino on 2015-10-19.
+//  Copyright © 2015 Milton and Parc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
